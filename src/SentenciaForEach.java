2f9f5ee69bbf9f307a1fdf0a4d61d6538763ab35
aespinosarskr <aespinosar96@gmail.com>
2023-01-09 20:46:13 -0600

    Sentencia ForEach

--- src/SentenciaForEach.java
+++ src/SentenciaForEach.java
@@ -1,3 +1,15 @@
+//ForEach manera simple de iterar sobre arreglos u objetos de colección de Java
 public class SentenciaForEach {
+    public static void main(String[] args) {
+        int[] numeros ={1, 3, 5, 7, 9, 11, 13, 15};
+        for (int num: numeros){
+            System.out.println("num = " + num);
+        }
+
+        String[] nombres = {"Andres", "Pepe", "Maria", "Paco", "Lalo", "Bea", "Pato", "Pepa"};
+        for (String nombre : nombres) {
+            System.out.println("nombre = " + nombre);
+        }
+    }
 
 }
