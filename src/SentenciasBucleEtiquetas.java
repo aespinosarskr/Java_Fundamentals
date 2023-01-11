66859390520783204a980dcc253b9e9db42ef38a
aespinosarskr <aespinosar96@gmail.com>
2023-01-10 18:46:51 -0600

    Sentencias bucle etiquetas

---
+++ src/SentenciasBucleEtiquetas.java
@@ -0,0 +1,28 @@
+public class SentenciasBucleEtiquetas {
+    public static void main(String[] args) {
+
+        bucle1:
+        for (int i = 0; i < 5; i++){
+
+            System.out.println();
+            for (int j = 0; j < 5; j++){
+                if (i == 2){
+                    continue bucle1;
+                }
+                System.out.println("[i = " + i + ", j = " + j + "], ");
+            }
+        }
+        System.out.println("================================================");
+        etiqueta:
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println();
+            for (int j = 0; j < 5; j++) {
+                if (i == 2) {
+                    break etiqueta;
+                }
+                System.out.println("[i = " + i + ", j = " + j + "], ");
+            }
+        }
+    }
+}
