d295fa527ef02d5752e71eb2dc11ddcb23d2f18c
aespinosarskr <aespinosar96@gmail.com>
2023-02-06 22:36:55 -0600

    Metodo getClass() y Clase Wrapper Boolean

---
+++ src/EjemploMetodoGetClass.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Method;
+
+public class EjemploMetodoGetClass {
+    public static void main(String[] args) {
+
+        String texto = "Hola que tal!";
+
+        Class strClass = texto.getClass();
+        System.out.println("strClass.getName() = " + strClass.getName());
+        System.out.println("strClass.getName() = " + strClass.getSimpleName());
+        System.out.println("strClass.getName() = " + strClass.getPackage().getName());
+        System.out.println("strClass.getName() = " + strClass);
+
+        for (Method metodo: strClass.getMethods()) {
+            System.out.println("metodo.getName() = " + metodo.getName());
+        }
+
+        Integer num = 34;
+        Class intClass = num.getClass();
+        Class objClass = intClass.getSuperclass().getSuperclass();
+        System.out.println("intClass = " + intClass.getSuperclass());
+        System.out.println("intClass = " + objClass);
+
+        for (Method metodo: objClass.getMethods()) {
+            System.out.println("metodo.getName() = " + metodo.getName());
+        }
+
+    }
+}
